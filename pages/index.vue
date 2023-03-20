edc86a52a235ec12508646c069d84ead491357ed
fx67ll <2717745014@qq.com>
2023-03-20 14:57:09 +0800

    initial PFUWZ -> JTCEE + LOTFE

---
+++ pages/index.vue
@@ -0,0 +1,44 @@
+<template>
+	<view class="fx67ll-nav-box">
+		<view class="fx67ll-title-box">fx67ll's uni-app 个人作品示例</view>
+		<view class="fx67ll-link-box">
+			<view class="fx67ll-nav-item"><uni-link :href="urlJTCEE" text="跨境速递演示项目示例" showUnderLine="false" font-size="20"></uni-link></view>
+			<view class="fx67ll-nav-item"><uni-link :href="urlLTOFE" text="彩票选号辅助工具示例" showUnderLine="false" font-size="20"></uni-link></view>
+			<view class="fx67ll-nav-item"><uni-link href="https://fx67ll.com" text="地图示例开发中，敬请期待" showUnderLine="false" font-size="20"></uni-link></view>
+		</view>
+		<fx67ll-footer />
+	</view>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			urlJTCEE: window.location.href + 'pages/JTCEE/login/login',
+			urlLTOFE: window.location.href + 'pages/LTOFE/index/index'
+		};
+	},
+	methods: {}
+};
+</script>
+
+<style lang="less">
+.fx67ll-nav-box {
+	padding: 80rpx 50rpx;
+	.fx67ll-title-box {
+		text-align: center;
+		font-size: 40rpx;
+		color: #2ECC71;
+	}
+	.fx67ll-link-box {
+		padding-top: 80rpx;
+		.fx67ll-nav-item {
+			text-align: center;
+			margin-bottom: 40rpx;
+		}
+		.fx67ll-nav-item:last-child {
+			margin-bottom: 0;
+		}
+	}
+}
+</style>
