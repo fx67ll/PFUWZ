d3588a000858102e01f5c041a3e64d176612073c
fx67ll <2717745014@qq.com>
2023-03-23 11:11:35 +0800

    H5 scan level up

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,7 @@
 	<view class="fx67ll-nav-box">
 		<view class="fx67ll-title-box">fx67ll's uni-app 个人作品示例</view>
 		<view class="fx67ll-link-box">
+			<view class="fx67ll-nav-item">H5二维码在线扫码示例</view>
 			<view class="fx67ll-nav-item"><uni-link :href="urlJTCEE" text="跨境速递演示项目示例" showUnderLine="false" font-size="20"></uni-link></view>
 			<view class="fx67ll-nav-item"><uni-link :href="urlLTOFE" text="彩票选号辅助工具示例" showUnderLine="false" font-size="20"></uni-link></view>
 			<view class="fx67ll-nav-item"><uni-link href="https://fx67ll.com" text="地图示例开发中，敬请期待" showUnderLine="false" font-size="20"></uni-link></view>
@@ -14,6 +15,7 @@
 export default {
 	data() {
 		return {
+			urlQR: window.location.href + 'pages/JTCEE/index/scan_qr_for_index',
 			urlJTCEE: window.location.href + 'pages/JTCEE/login/login',
 			urlLTOFE: window.location.href + 'pages/LTOFE/index/index'
 		};
@@ -28,13 +30,16 @@ export default {
 	.fx67ll-title-box {
 		text-align: center;
 		font-size: 40rpx;
-		color: #2ECC71;
+		color: #2ecc71;
 	}
 	.fx67ll-link-box {
 		padding-top: 80rpx;
 		.fx67ll-nav-item {
 			text-align: center;
 			margin-bottom: 40rpx;
+			// 仿uni-link样式
+			color: rgb(153, 153, 153);
+			font-size: 20px;
 		}
 		.fx67ll-nav-item:last-child {
 			margin-bottom: 0;
